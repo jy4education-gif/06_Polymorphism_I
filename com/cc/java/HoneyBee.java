a2bc252354f127681c74be57165719a02adeb7f9
jy4education_git <jy4education@gmail.com>
2026-01-30 09:28:09 +0100

    +

---
+++ com/cc/java/HoneyBee.java
@@ -0,0 +1,14 @@
+package com.cc.java;
+
+public class HoneyBee{
+    
+   public String doYourJob(){
+    return "I'm a HoneyBEE!!";
+   } 
+
+   puplic String fly(){
+    return "I can fly!!";
+   }
+
+
+}
