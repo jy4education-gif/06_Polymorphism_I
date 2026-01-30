9b0acaa2ea0aeb71690f80b721d0784cdbd68896
jy4education_git <jy4education@gmail.com>
2026-01-30 10:46:53 +0100

    +

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -25,29 +25,40 @@ public class App {
         pollObject(queen);
         pollObject(worker);
         pollObject(drone);
+ 
+        //-- Das sollte eigentlich nicht gehen!--//
+        HoneyBee bee = new HoneyBee(); // SuperKlasse!
+        pollObject(bee);
 
     }
 
-    /* 2. Iteration */
+    /* 2. Iteration: überladene Methoden */
 
-    private static void pollObject(Queen obj){
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("-------------------");                  // pollObject: Overloading
-    }
+    // private static void pollObject(Queen obj){
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("-------------------");                  // pollObject: Overloading
+    // }
 
-     private static void pollObject(Worker obj){
+    //  private static void pollObject(Worker obj){
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("-------------------");
+    // }
+
+    //  private static void pollObject(Drone obj){
+    //     output(obj.doYourJob());
+    //     output(obj.fly());
+    //     output("-------------------");
+    // }
+
+    /* 3. Iteration: Polymorphie */
+
+     private static void pollObject(HoneyBee obj){
         output(obj.doYourJob());
         output(obj.fly());
         output("-------------------");
     }
-
-     private static void pollObject(Drone obj){
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("-------------------");
-    }
-
 
 
     private static void output(String outputStr) {
